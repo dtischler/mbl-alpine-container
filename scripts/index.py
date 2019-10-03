9abaa2c689471e9b66d70c8b992a1f8247deb338
Brian Boles <briboles@yahoo.com>
2019-10-02 20:38:49 -0700

    update py script

--- scripts/index.py
+++ scripts/index.py
@@ -1,15 +1,38 @@
 import qwiic
 import time
-import logging
 import gps
-from influxdb import InfluxDBClient
+import json
+import paho.mqtt.client as mqtt
 from datetime import datetime
 
-# Wait for influxd to start up.
-time.sleep(10)
+import paho.mqtt.client as mqtt
 
-client = InfluxDBClient(host='localhost', port=8086)
-client.create_database('iot-weather')
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("iot-weather")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+def on_publish(client, userdata, mid):
+    print("Message Publisted "+str(mid))
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
+mqttclient = mqtt.Client()
+mqttclient.on_log = on_log
+mqttclient.on_connect = on_connect
+mqttclient.on_message = on_message
+mqttclient.on_publish = on_publish
+mqttclient.username_pw_set("device", password="eDFm3exZ2aPHs4")
+mqttclient.loop_start()
+mqttclient.connect("edge-us-az-1.iot.unfinishedcode.net", 1883, 60)
 
 CCS811 = qwiic.QwiicCcs811()
 BME280 = qwiic.QwiicBme280() 
@@ -33,60 +56,64 @@ tags = {
 time.sleep(10)
 
 while True:
-    CCS811.read_algorithm_results()
-    date = datetime.utcnow().isoformat(' ')
-    
-    json_body = [
-        {
-            "measurement": "CO2",
-            "tags": tags,
-            "time": date,
-            "fields": {
-                "value": CCS811.CO2,
+    try:
+        CCS811.read_algorithm_results()
+        date = datetime.utcnow().isoformat(' ')
+        
+        json_body = [
+            {
+                "measurement": "CO2",
+                "tags": tags,
+                "time": date,
+                "fields": {
+                    "value": CCS811.CO2,
+                },
             },
-        },
-        {
-            "measurement": "tVOC",
-            "tags": tags,
-            "time": date,
-            "fields": {
-                "value": CCS811.TVOC,
+            {
+                "measurement": "tVOC",
+                "tags": tags,
+                "time": date,
+                "fields": {
+                    "value": CCS811.TVOC,
+                },
             },
-        },
-        {
-            "measurement": "fahrenheit",
-            "tags": tags,
-            "time": date,
-            "fields": {
-                "value": BME280.get_temperature_fahrenheit(),
+            {
+                "measurement": "fahrenheit",
+                "tags": tags,
+                "time": date,
+                "fields": {
+                    "value": BME280.get_temperature_fahrenheit(),
+                },
             },
-        },
-        {
-            "measurement": "celsius",
-            "tags": tags,
-            "time": date,
-            "fields": {
-                "value": BME280.get_temperature_celsius(),
+            {
+                "measurement": "celsius",
+                "tags": tags,
+                "time": date,
+                "fields": {
+                    "value": BME280.get_temperature_celsius(),
+                },
             },
-        },
-        {
-            "measurement": "pressure",
-            "tags": tags,
-            "time": date,
-            "fields": {
-                "value": BME280.pressure,
+            {
+                "measurement": "pressure",
+                "tags": tags,
+                "time": date,
+                "fields": {
+                    "value": BME280.pressure,
+                },
             },
-        },
-        {
-            "measurement": "humidity",
-            "tags": tags,
-            "time": date,
-            "fields": {
-                "value": BME280.humidity,
+            {
+                "measurement": "humidity",
+                "tags": tags,
+                "time": date,
+                "fields": {
+                    "value": BME280.humidity,
+                },
             },
-        },
-    ]
+        ]
 
-    print("Writing Data to infuxDB")
-    client.write_points(json_body, database="iot-weather")
-    time.sleep(60)
+        res = mqttclient.publish("iot-weather", payload=json.dumps(json_body), qos=2)
+        time.sleep(60)
+    except Exception as err:
+        print("An error occurred. Error details:")
+        print(err.args)
+        time.sleep(60)
